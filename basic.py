b30c93d80fd3805c86b4e16d2cd78cbef0a6a416
Karthik3224 <153287084+Karthik3224@users.noreply.github.com>
2023-12-14 20:23:32 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,65 @@
+print("karthik")
+# variables
+x=10 
+y="karthik"
+print(x)
+print(y)
+# datat types
+a=10
+print(type(a))
+b="mani"
+print(type(b))
+c=12.5
+print(type(c))
+d=True
+print(type(d))
+#if else condition
+e=10
+f=12
+if e>f:
+    print("e is grater than f")
+else:
+    print("f is greater than e")
+#operators
+#arthimetic operators
+num1=10
+num2=20
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 % num2)
+print(num1 ** num2)
+#assinment operator'
+k=5
+print(k)
+k+=3
+print(k)
+k-=3
+print(k)
+k*=5
+print(k)
+k/=2
+print(k)
+# comparision operator
+num3=6
+num4=8
+print(num3 == num4)
+print(num3 != num4)
+print(num3 < num4)
+print(num3 > num4)
+print(num3 < num4)
+print(num3 <= num4)
+print(num3 >= num4)
+# logical operator
+x < 5 and  x < 10	
+x < 5 or x < 4
+not(x < 5 and x < 10)
+# for loop
+for x in range(6):
+  print(x)
+# while loop
+i = 1
+while i < 6:
+  print(i)
+  i += 1
